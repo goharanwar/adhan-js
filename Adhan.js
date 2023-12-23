c0cd55f4ec7588b7a97990db9bb27aad22fba23f
Gohar Anwar <gohar@goharanwar.com>
2023-12-23 14:49:04 +0500

    chore: add compiled library

--- Adhan.js
+++ Adhan.js
@@ -1127,14 +1127,14 @@ function qibla(coordinates) {
 
 
 class SunnahTimes_SunnahTimes {
-  constructor(prayerTimes) {
+  constructor(prayerTimes, precise = false) {
     const date = prayerTimes.date;
     const nextDay = dateByAddingDays(date, 1);
     const nextDayPrayerTimes = new PrayerTimes_PrayerTimes(prayerTimes.coordinates, nextDay, prayerTimes.calculationParameters, prayerTimes.precise);
     const nightDuration = (nextDayPrayerTimes.fajr.getTime() - prayerTimes.maghrib.getTime()) / 1000.0;
-    this.middleOfTheNight = roundedMinute(dateByAddingSeconds(prayerTimes.maghrib, nightDuration / 2));
-    this.lastThirdOfTheNight = roundedMinute(dateByAddingSeconds(prayerTimes.maghrib, nightDuration * (2 / 3)));
-    this.lastSixthOfTheNight = roundedMinute(dateByAddingSeconds(prayerTimes.maghrib, nightDuration * (5 / 6)));
+    this.middleOfTheNight = precise ? dateByAddingSeconds(prayerTimes.maghrib, nightDuration / 2) : roundedMinute(dateByAddingSeconds(prayerTimes.maghrib, nightDuration / 2));
+    this.lastThirdOfTheNight = precise ? dateByAddingSeconds(prayerTimes.maghrib, nightDuration * (2 / 3)) : roundedMinute(dateByAddingSeconds(prayerTimes.maghrib, nightDuration * (2 / 3)));
+    this.lastSixthOfTheNight = precise ? dateByAddingSeconds(prayerTimes.maghrib, nightDuration * (5 / 6)) : roundedMinute(dateByAddingSeconds(prayerTimes.maghrib, nightDuration * (5 / 6)));
   }
 }
 // CONCATENATED MODULE: ./src/Adhan.js
